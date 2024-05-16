dcb087a0d07c9a2bec7c8f85f55eacb9ba4c4530
Samantha Samarua <samantha.samarua@gmail.com>
2024-05-16 23:24:27 +0930

    auth.js: Update the auth middleware function to work with the GraphQL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,8 +6,13 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    }
+  }),
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function (req ) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -25,11 +31,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      
     }
 
     // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
